2aaf433157715b844081565bdb2d161e15ef1259
SadanandKallakuri <sadanand2u@gmail.com>
2017-07-05 00:13:23 -0500

    commit

---
+++ Lab Assignment 4- WordNet- ConceptNet5- OpenIE- LDA/Source/Lab 4/src/main/scala/lda.scala
@@ -0,0 +1,205 @@
+package lda
+
+import java.io.PrintStream
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.mllib.clustering.{DistributedLDAModel, EMLDAOptimizer, LDA, OnlineLDAOptimizer}
+import org.apache.spark.mllib.feature.{HashingTF, IDF}
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import scopt.OptionParser
+
+/**
+  * An example Latent Dirichlet Allocation (LDA) app. Run with
+  * {{{
+  * ./bin/run-example mllib.LDAExample [options] <input>
+  * }}}
+  * If you use it as a template to create your own app, please use `spark-submit` to submit your app.
+  */
+object SparkLDAMain {
+
+  private case class Params (
+                              input: Seq[String] = Seq.empty,
+                              k: Int = 20,
+                              algorithm: String = "em")
+
+  def main(args: Array[String]) {
+    val defaultParams = Params()
+
+    val parser = new OptionParser[Params]("LDAExample") {
+      head("LDAExample: an example LDA app for plain text data.")
+      opt[Int]("k")
+        .text(s"number of topics. default: ${defaultParams.k}")
+        .action((x, c) => c.copy(k = x))
+      opt[String]("algorithm")
+        .text(s"inference algorithm to use. em and online are supported." +
+          s" default: ${defaultParams.algorithm}")
+        .action((x, c) => c.copy(algorithm = x))
+      arg[String]("<input>...")
+        .text("input paths (directories) to plain text corpora." +
+          "  Each text file line should hold 1 document.")
+        .unbounded()
+        .required()
+        .action((x, c) => c.copy(input = c.input :+ x))
+    }
+
+    parser.parse(args, defaultParams).map { params =>
+      run(params)
+    }.getOrElse {
+      parser.showUsageAsError
+      sys.exit(1)
+    }
+  }
+
+  private def run(params: Params) {
+    System.setProperty("hadoop.home.dir", "C:\\winutils")
+    val conf = new SparkConf().setAppName(s"LDAExample with $params").setMaster("local[*]").set("spark.driver.memory", "4g").set("spark.executor.memory", "4g")
+    val sc = new SparkContext(conf)
+
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val topic_output = new PrintStream("data/Results.txt")
+    // Load documents, and prepare them for LDA.
+    val preprocessStart = System.nanoTime()
+    val (corpus, vocabArray, actualNumTokens) =
+      preprocess(sc, params.input)
+    corpus.cache()
+    val actualCorpusSize = corpus.count()
+    val actualVocabSize = vocabArray.length
+    val preprocessElapsed = (System.nanoTime() - preprocessStart) / 1e9
+
+    println()
+    println(s"Corpus summary:")
+    println(s"\t Training set size: $actualCorpusSize documents")
+    println(s"\t Vocabulary size: $actualVocabSize terms")
+    println(s"\t Training set size: $actualNumTokens tokens")
+    println(s"\t Preprocessing time: $preprocessElapsed sec")
+    println()
+
+
+    topic_output.println()
+    topic_output.println(s"Corpus summary:")
+    topic_output.println(s"\t Training set size: $actualCorpusSize documents")
+    topic_output.println(s"\t Vocabulary size: $actualVocabSize terms")
+    topic_output.println(s"\t Training set size: $actualNumTokens tokens")
+    topic_output.println(s"\t Preprocessing time: $preprocessElapsed sec")
+    topic_output.println()
+
+    // Run LDA.
+    val lda = new LDA()
+
+    val optimizer = params.algorithm.toLowerCase match {
+      case "em" => new EMLDAOptimizer
+      // add (1.0 / actualCorpusSize) to MiniBatchFraction be more robust on tiny datasets.
+      case "online" => new OnlineLDAOptimizer().setMiniBatchFraction(0.05 + 1.0 / actualCorpusSize)
+      case _ => throw new IllegalArgumentException(
+        s"Only em, online are supported but got ${params.algorithm}.")
+    }
+
+    lda.setOptimizer(optimizer)
+      .setK(params.k)
+      .setMaxIterations(50)
+
+    val startTime = System.nanoTime()
+    val ldaModel = lda.run(corpus)
+    val elapsed = (System.nanoTime() - startTime) / 1e9
+
+    println(s"Finished training LDA model.  Summary:")
+    println(s"\t Training time: $elapsed sec")
+
+
+    topic_output.println(s"Finished training LDA model.  Summary:")
+    topic_output.println(s"\t Training time: $elapsed sec")
+
+    if (ldaModel.isInstanceOf[DistributedLDAModel]) {
+      val distLDAModel = ldaModel.asInstanceOf[DistributedLDAModel]
+      val avgLogLikelihood = distLDAModel.logLikelihood / actualCorpusSize.toDouble
+      println(s"\t Training data average log likelihood: $avgLogLikelihood")
+      println()
+      topic_output.println(s"\t Training data average log likelihood: $avgLogLikelihood")
+      topic_output.println()
+    }
+
+    // Print the topics, showing the top-weighted terms for each topic.
+    val topicIndices = ldaModel.describeTopics(maxTermsPerTopic = actualVocabSize)
+    val topics = topicIndices.map { case (terms, termWeights) =>
+      terms.zip(termWeights).map { case (term, weight) => (vocabArray(term.toInt), weight) }
+    }
+    println(s"${params.k} topics:")
+    topic_output.println(s"${params.k} topics:")
+    topics.zipWithIndex.foreach { case (topic, i) =>
+      println(s"TOPIC $i")
+      // topic_output.println(s"TOPIC $i")
+      topic.foreach { case (term, weight) =>
+        println(s"$term\t$weight")
+        topic_output.println(s"TOPIC_$i;$term;$weight")
+      }
+      println()
+      topic_output.println()
+    }
+    topic_output.close()
+    sc.stop()
+  }
+
+  /**
+    * Load documents, tokenize them, create vocabulary, and prepare documents as term count vectors.
+    *
+    * @return (corpus, vocabulary as array, total token count in corpus)
+    */
+  private def preprocess(sc: SparkContext,paths: Seq[String]): (RDD[(Long, Vector)], Array[String], Long) = {
+
+    //Reading Stop Words
+    val stopWords=sc.textFile("data/stopwords.txt").collect()
+    val stopWordsBroadCast=sc.broadcast(stopWords)
+    //System.out.println("broadcast"+stopWordsBroadCast)
+    //val kk=sc.textFile(paths.mkString(","))
+    //kk.saveAsTextFile("kk")
+
+
+    val df = sc.textFile(paths.mkString(",")).map(f => {
+      //val lemmatised=CoreNLP.returnLemma(f)
+      val splitString = f.split(" ")
+      splitString
+    })
+
+    val stopWordRemovedDF=df.map(f=>{
+      //Filtered numeric and special characters out
+      val filteredF=f.map(_.replaceAll("[^a-zA-Z]"," "))
+        //Filter out the Stop Words
+        .filter(ff=>{
+        if(stopWordsBroadCast.value.contains(ff.toLowerCase))
+          false
+        else
+          true
+      })
+      filteredF
+    })
+    //val dfseq1=df.map(_.toSeq)
+    val dfseq=df.map(_.toSeq)
+    System.out.println("seqqqqq")
+    dfseq.take(5).foreach(println)
+    //Creating an object of HashingTF Class
+    val hashingTF = new HashingTF(df.count().toInt)  // VectorSize as the Size of the Vocab
+    System.out.println(hashingTF)
+
+    //Creating Term Frequency of the document
+    val tf = hashingTF.transform(dfseq)
+    System.out.println("tfffffffffff")
+    tf.take(5).foreach(println)
+    tf.cache()
+
+    val idf = new IDF().fit(tf)
+
+    //Creating Inverse Document Frequency
+    val tf7=tf.zipWithIndex().map(_.swap)
+    val tfidf = idf.transform(tf).zipWithIndex().map(_.swap)
+    System.out.println("tfidffffff")
+    tfidf.take(5).foreach(println)
+
+    val dff= df.flatMap(f=>f)
+    val vocab=dff.distinct().collect()
+    (tf7, vocab, dff.count()) // Vector, Vocab, total token count
+  }
+}
+
+
---
+++ Lab Assignment 4- WordNet- ConceptNet5- OpenIE- LDA/Source/Lab 4/src/main/scala/wordnet/WordNetSpark.scala
@@ -0,0 +1,24 @@
+package wordnet
+import org.apache.spark.{SparkConf, SparkContext}
+import rita.RiWordNet
+object WordNetSpark {
+  def main(args: Array[String]): Unit = {
+    System.setProperty("hadoop.home.dir", "C:\\winutils")
+    val conf = new SparkConf().setAppName("WordNetSparkDataset").setMaster("local[*]").set("spark.driver.memory", "4g").set("spark.executor.memory", "4g")
+    val sc = new SparkContext(conf)
+    val dataset=sc.textFile("data/a.txt")
+    val syn=dataset.map(g=>{
+      val wordnet1 = new RiWordNet("C:\\Users\\sadan\\Desktop\\KDM\\4\\WordNet-3.0")
+      val farr1=g.split(" ")
+      getSynoymns(wordnet1,"person")
+    })
+    syn.take(1).foreach(g=>println(g.mkString(" ")))
+  }
+  def getSynoymns(wordnet1:RiWordNet,word:String): Array[String] ={
+    println(word)
+    val pos=wordnet1.getPos(word)
+    println(pos.mkString(" "))
+    val syn=wordnet1.getAllSynonyms(word, pos(0), 10)
+    syn
+  }
+}
